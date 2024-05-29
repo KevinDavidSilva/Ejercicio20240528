0b434ca2db5d4ab3819ff0c4fe4a26c52e64eea0
KevinSilva <kevinsilvadb503@gmail.com>
2024-05-29 10:26:51 -0600

    Clase producto

---
+++ src/Productos.java
@@ -0,0 +1,58 @@
+public class Productos {
+    private int id;
+    private String nombre;
+    private String marca;
+    private double precio;
+    
+    public Productos() {
+    }
+
+    public Productos(int id, String nombre, String marca, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", marca='" + marca + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
